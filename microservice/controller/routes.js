b6ae508494497a6ccdae82f5bee7e9e5dfcb40d9
AndreasB2B <99027014+AndreasB2B@users.noreply.github.com>
2023-05-19 23:05:20 +0200

    ingress3

--- microservice/controller/routes.js
+++ microservice/controller/routes.js
@@ -2,7 +2,7 @@ import asd from '../services/middleware/rabbitMQService'
 
 module.exports = function(app){
     app.get("/send-msg")
-    app.get("/ms/ticketservice", (req,res)=>{
+    app.get("/ms/ticketservice/", (req,res)=>{
         res.send("Welcome to Ticket Service")
     })
 }
