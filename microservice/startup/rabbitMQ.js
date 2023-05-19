c929bdf22594e42fb7386addbc1fbe821c8b5d24
AndreasB2B <99027014+AndreasB2B@users.noreply.github.com>
2023-05-20 00:18:23 +0200

    ingress5

--- microservice/startup/rabbitMQ.js
+++ microservice/startup/rabbitMQ.js
@@ -1,27 +1,27 @@
-const amqp = require("amqplib")
+// const amqp = require("amqplib")
 
-let channel, connection;
+// let channel, connection;
 
-async function connectQueue(){
-    try {
-        connection = process.env.ENVIRONTMENT == 'prod'? await amqp.connect(`amqp://${process.env.RABBITMQ_IP}:5672`) : await amqp.connect("amqp://localhost:5672");
-        channel    = await connection.createChannel()
+// async function connectQueue(){
+//     try {
+//         connection = process.env.ENVIRONTMENT == 'prod'? await amqp.connect(`amqp://${process.env.RABBITMQ_IP}:5672`) : await amqp.connect("amqp://localhost:5672");
+//         channel    = await connection.createChannel()
 
-        await channel.assertQueue("test-queue")
-    } catch (error) {
-        console.log(error)
-    }
-}
+//         await channel.assertQueue("test-queue")
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
-connectQueue()
+// connectQueue()
 
-async function sendData (data) {
-    await channel.sendToQueue("test-queue", Buffer.from(JSON.stringify(data)));
+// async function sendData (data) {
+//     await channel.sendToQueue("test-queue", Buffer.from(JSON.stringify(data)));
 
-    await channel.close();
-    await connection.close();
-}
+//     await channel.close();
+//     await connection.close();
+// }
 
-module.exports = {
-    sendData
-}
+// module.exports = {
+//     sendData
+// }
